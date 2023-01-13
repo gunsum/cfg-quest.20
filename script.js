f654955fa6b54c63e328b74c239ad1afd61834f0
Fatma Oymak <bedizma@hotmail.com>
2023-01-13 21:01:43 +0000

    answer is assignment error

---
+++ script.js
@@ -0,0 +1,16 @@
+var readingList = [
+    "The Grapes of Wrath",
+    "Dune",
+    "Anna Karenina",
+]
+
+readingList.push("The Myth of Sisyphus")
+readingList.push("The Stranger")
+readingList.pop()
+readingList.shift()
+
+console.log(readingList)
+
+//push put the element end of the list
+//shift (remove) the first element of the array
+// ensonunda ; olmadigi icin assignment error verir
